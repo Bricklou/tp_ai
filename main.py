ca1773a5a769e751fe99276fd646b37ab31fbd74
Louis Bailleau <louis13.bailleau@gmail.com>
2024-11-07 14:39:11 +0100

    feat: exercice with linear regression

--- main.py
+++ main.py
@@ -1,38 +1,24 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
-from sklearn.metrics import confusion_matrix
-from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.linear_model import LinearRegression
 
-train_data = pd.read_csv("dataset/fraud_train.csv")
-test_data = pd.read_csv("dataset/fraud_test.csv")
+train_data = pd.read_csv("dataset/salary_train.csv")
+test_data = pd.read_csv("dataset/salary_test.csv")
 
-X_train = train_data.drop("is_fraud", axis=1)
-Y_train = train_data["is_fraud"]
+X_train = train_data.drop("salary", axis=1)
+Y_train = train_data["salary"]
 
-X_test = test_data.drop("is_fraud", axis=1)
-Y_test = test_data["is_fraud"]
+X_test = test_data.drop("salary", axis=1)
+Y_test = test_data["salary"]
 
-model = DecisionTreeClassifier(max_depth=5, random_state=42)
+model = LinearRegression()
 model.fit(X_train, Y_train)
 
 Y_pred = model.predict(X_test)
 
-plt.figure(figsize=(50, 50))
-plot_tree(
-    model,
-    feature_names=X_train.columns,
-    class_names=["Not fraud", "Fraud"],
-    filled=True,
-    rounded=True,
-)
-plt.title("Decision Tree for Fraud Detection")
-plt.show()
-
-# Plot confusion matrix
-confusion_matrix = confusion_matrix(Y_test, Y_pred)
-sns.heatmap(confusion_matrix, annot=True, fmt="d", cmap="Blues")
-plt.title("Confusion Matrix")
-plt.ylabel("True label")
-plt.xlabel("Predicted label")
+plt.scatter(X_test["years_of_experience"], Y_test)
+plt.plot(X_test["years_of_experience"], Y_pred, color="red")
+plt.title("Linear Regression of salary over years of experience")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
 plt.show()
