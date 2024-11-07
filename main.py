4707640e58a9749f09996222daef22992e472621
Louis Bailleau <louis13.bailleau@gmail.com>
2024-11-07 14:56:55 +0100

    feat: exercice with unsupervised clustering

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
-import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
 
-train_data = pd.read_csv("dataset/salary_train.csv")
-test_data = pd.read_csv("dataset/salary_test.csv")
+raw_data = pd.read_csv("dataset/customer_train.csv")
 
-X_train = train_data.drop("salary", axis=1)
-Y_train = train_data["salary"]
+scaler = StandardScaler()
+features_scaled = scaler.fit_transform(raw_data)
 
-X_test = test_data.drop("salary", axis=1)
-Y_test = test_data["salary"]
+data = pd.DataFrame(features_scaled, columns=raw_data.columns)
 
-model = LinearRegression()
-model.fit(X_train, Y_train)
+k = 4
+model = KMeans(n_clusters=k, random_state=42)
+model.fit(raw_data)
 
-Y_pred = model.predict(X_test)
+data["cluster"] = model.fit_predict(data)
 
-plt.scatter(X_test["years_of_experience"], Y_test)
-plt.plot(X_test["years_of_experience"], Y_pred, color="red")
-plt.title("Linear Regression of salary over years of experience")
-plt.xlabel("Years of Experience")
-plt.ylabel("Salary")
+plt.figure(figsize=(15, 5))
+
+# First subplot
+plt.subplot(1, 3, 1)
+plt.scatter(data["annual_income"], data["spending_score"], c=data["cluster"])
+plt.xlabel("Annual income")
+plt.ylabel("Spending Score")
+plt.title("Income VS Spending Score")
+
+plt.subplot(1, 3, 2)
+plt.scatter(data["age"], data["spending_score"], c=data["cluster"])
+plt.xlabel("Age")
+plt.ylabel("Spending Score")
+plt.title("Age VS Spending Score")
+
+plt.subplot(1, 3, 3)
+plt.scatter(data["age"], data["annual_income"], c=data["cluster"])
+plt.xlabel("Age")
+plt.ylabel("Annual income")
+plt.title("Age VS Annual Income")
+
+plt.tight_layout()
 plt.show()
